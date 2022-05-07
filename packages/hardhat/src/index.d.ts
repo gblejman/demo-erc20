301afa830c036f9f675b022d912eb0137af0c7f8
g <gblejman@gmail.com>
2022-05-06 22:09:25 -0300

    again

--- packages/hardhat/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export type { ERC20 } from "../typechain";
-export type { ERC20Faucet } from "../typechain";
--- packages/hardhat/src/index.ts
+++ packages/hardhat/src/index.ts
@@ -1,5 +1,6 @@
 import erc20 from "../artifacts/contracts/erc20/ERC20.sol/ERC20.json";
 import erc20Faucet from "../artifacts/contracts/faucet/ERC20Faucet.sol/ERC20Faucet.json";
+
 export type { ERC20 } from "../typechain";
 export type { ERC20Faucet } from "../typechain";
 
